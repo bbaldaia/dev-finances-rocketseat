94d90131289fa871a2762e25dbce8d9b526a91dd
bbaldaia <brunobfreire98@gmail.com>
2021-09-26 17:20:18 -0300

    Avançando com o JavaScript

---
+++ scripts.js
@@ -0,0 +1,83 @@
+const Modal = {
+    open() {
+        document.querySelector('.modal-overlay').classList.add('active')
+    },
+
+    close() {
+        document.querySelector('.modal-overlay').classList.remove('active')
+    }
+}
+
+const transactions = [
+    {
+        id: 1,
+        description: 'Luz',
+        amount: -50000,
+        date: '23/01/2021'
+    },
+    {
+        id: 2,
+        description: 'Website',
+        amount: 500000,
+        date: '23/01/2021'
+    },
+    {
+        id: 3,
+        description: 'Internet',
+        amount: -20000,
+        date: '23/01/2021',
+    }
+]
+
+const Transaction = {
+
+    incomes() {
+        //somar as entradas
+    },
+
+    expenses() {
+        //somar as saídas
+    },
+
+    total() {
+        //entradas - saídas
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector('#data-table tbody'),
+    
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+        // Adicionando dentro do recém criado tr (table row) os 
+        // elementos td que estão na função innerHTMLTransaction
+        DOM.transactionsContainer.appendChild(tr)
+    },
+
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+        const amount = Utils.formatCurrency(transaction.amount)
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover transação">
+            </td>
+        `
+        // Retornando os elementos td. Nesse caso precisa ter o mesmo nome da constante
+        return html
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : ""
+    }
+}
+
+transactions.forEach(function(transaction) {
+    DOM.addTransaction(transaction)
+})
+
