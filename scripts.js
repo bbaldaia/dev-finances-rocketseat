523326ef5ff4aa2da07398db929b1102490d7a1e
bbaldaia <brunobfreire98@gmail.com>
2021-09-29 16:34:49 -0300

    Mais implementações no Javascript

--- scripts.js
+++ scripts.js
@@ -8,39 +8,66 @@ const Modal = {
     }
 }
 
-const transactions = [
-    {
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021'
-    },
-    {
-        id: 2,
-        description: 'Website',
-        amount: 500000,
-        date: '23/01/2021'
-    },
-    {
-        id: 3,
-        description: 'Internet',
-        amount: -20000,
-        date: '23/01/2021',
-    }
-]
-
 const Transaction = {
+    all: [
+        {
+            description: 'Luz',
+            amount: -50001,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Website',
+            amount: 500000,
+            date: '23/01/2021'
+        },
+        {
+            description: 'Internet',
+            amount: -20012,
+            date: '23/01/2021',
+        },
+        {
+            description: 'App',
+            amount: 200000,
+            date: '23/01/2021',
+        }
+    ],    
+
+    add(transaction) {
+        Transaction.all.push(transaction)
+
+        App.reload()
+    },
 
     incomes() {
-        //somar as entradas
+        let income = 0
+        //pegar todas as transacoes 
+        //para cada transacao,
+        transactions.forEach(transaction =>  {
+            //se ela for maior que zero 
+            if (transaction.amount > 0) {
+                //somar a uma variavel e retornar a variavel 
+                income += transaction.amount
+            }
+        })
+        return income
     },
 
     expenses() {
-        //somar as saídas
+        let expense = 0
+        //pegar todas as transacoes 
+        //para cada transacao,
+        transactions.forEach(transaction => {
+            //se ela for menor que zero
+            if (transaction.amount < 0) {
+                //somar a uma variavel e retornar a variavel
+                expense += transaction.amount
+            }
+        })
+        return expense
     },
 
     total() {
-        //entradas - saídas
+        return Transaction.incomes() + Transaction.expenses()
     }
 }
 
@@ -60,7 +87,7 @@ const DOM = {
         const amount = Utils.formatCurrency(transaction.amount)
         const html = `
             <td class="description">${transaction.description}</td>
-            <td class="${CSSclass}">${transaction.amount}</td>
+            <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
                 <img src="./assets/minus.svg" alt="Remover transação">
@@ -68,16 +95,75 @@ const DOM = {
         `
         // Retornando os elementos td. Nesse caso precisa ter o mesmo nome da constante
         return html
+    },
+
+    updateBalance() {
+        document
+                .getElementById('incomeDisplay')
+                .innerHTML = Utils.formatCurrency(Transaction.incomes())
+        document
+                .getElementById('expenseDisplay')
+                .innerHTML = Utils.formatCurrency(Transaction.expenses())
+        document
+                .getElementById('totalDisplay')
+                .innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
     }
 }
 
 const Utils = {
     formatCurrency(value) {
         const signal = Number(value) < 0 ? "-" : ""
+        value = String(value).replace(/\D/g, "")
+        value = Number(value) / 100
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        })
+        return signal + value
     }
 }
 
-transactions.forEach(function(transaction) {
-    DOM.addTransaction(transaction)
-})
+const Form = {    
 
+    formatData() {
+        console.log('formatei')
+    },
+
+    validateFields() {
+        console.log('validei')
+    },
+    
+    submit(event) {
+        event.preventDefault()
+
+        //validar se todos os campos do modal foram preenchidos
+        Form.validateFields()
+        //formatar os dados para salvar
+        Form.formatData()
+    }
+}
+
+const App = {
+    init() {
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+        DOM.updateBalance
+    },
+
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    },
+}
+
+App.init()
+
+
+//pesquisar arrow function
+//foreach
+//e dois pontos no javascript
